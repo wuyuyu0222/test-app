e9219030e9bea93e4b452b6de519b7b612238797
jakewu0222 <wuyuyu0222@gmail.com>
2019-01-02 17:47:20 +0800

    Form with display table

--- src/component/Form.js
+++ src/component/Form.js
@@ -6,7 +6,8 @@ export class Form extends Component {
     this.state = {
       name: '',
       gender: '',
-      age: 0
+      age: 0,
+      personList: []
     };
   }
 
@@ -17,41 +18,87 @@ export class Form extends Component {
   }
 
   handleSubmit = (e) => {
+    this.state.personList.push({
+      name: this.state.name,
+      gender: this.state.gender,
+      age: this.state.age
+    });
+    this.setState({
+      name: '',
+      gender: '',
+      age: 0
+    });
     console.log(this.state);
     e.preventDefault();
   }
 
+  displayTable = () => {
+    let personList = [];
+    if (this.state.personList.length > 0) {
+      personList = this.state.personList.reverse().map((person, i) => {
+        return (
+          <tr key={i}>
+            <td>{person.name || 'Null'}</td>
+            <td>{person.gender || 'Null'}</td>
+            <td>{person.age || 'Null'}</td>
+          </tr>
+        );
+      });
+    }
+    return (
+      <table>
+        <thead>
+          <tr>
+            <th>Name</th>
+            <th>Gender</th>
+            <th>Age</th>
+          </tr>
+        </thead>
+        <tbody>
+          {personList}
+        </tbody>
+      </table>
+    );
+  }
+
   render() {
     return (
-      <div>
-        <form onSubmit={this.handleSubmit}>
-          <div>
-            <span>Name: </span>
-            <input type="text" name="name" value={this.state.name}
-              onChange={this.handleInput} />
+      <div className="row">
+        <form className="col s12" onSubmit={this.handleSubmit}>
+          <div className="row">
+            <div className="input-field col s12">
+              <input type="text" id="input-name"
+                name="name" value={this.state.name}
+                onChange={this.handleInput} />
+              <label htmlFor="input-name">Name</label>
+            </div>
+            <div className="input-field col s12">
+              <input type="number" id="input-age"
+                name="age" value={this.state.age}
+                onChange={this.handleInput} />
+              <label htmlFor="input-age">age</label>
+            </div>
+            <div className="input-field col s12">
+              <p className="left">
+                <label className="mr-2x">
+                  <input type="radio" name="gender" value="male"
+                    onChange={this.handleInput}
+                    checked={this.state.gender === "male"} />
+                  <span>Male</span>
+                </label>
+                <label>
+                  <input type="radio" name="gender" value="female"
+                    onChange={this.handleInput}
+                    checked={this.state.gender === "female"} />
+                  <span>Female</span>
+                </label>
+              </p>
+            </div>
           </div>
-          <div>
-            <span>Gender: </span>
-            <input type="radio" name="gender" value="male"
-              checked={this.state.gender === "male"}
-              onChange={this.handleInput} />
-            <input type="radio" name="gender" value="female"
-              checked={this.state.gender === "female"}
-              onChange={this.handleInput} />
-          </div>
-          <div>
-            <span>Age: </span>
-            <input type="number" name="age" value={this.state.age}
-              onChange={this.handleInput} />
-          </div>
-          <input type="submit" value="submit" />
+          <input type="submit" value="submit" className="btn" />
         </form>
-        <div>
-          <div>
-            <p>Name: {this.state.name}</p>
-            <p>Gender: {this.state.gender}</p>
-            <p>Age: {this.state.age}</p>
-          </div>
+        <div className="col s12">
+          {this.displayTable()}
         </div>
       </div>
     )
